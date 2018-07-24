4f0f656024eb4d954d054aa96b6d51e500cad531
Laurent George <laurent.f.george@gmail.com>
2018-07-24 22:15:45 +0200

    first exercise

--- course/02_category_widget/task_02_category_widget/lib/category.dart
+++ course/02_category_widget/task_02_category_widget/lib/category.dart
@@ -6,6 +6,8 @@
 // https://www.dartlang.org/guides/language/effective-dart/style#ordering
 import 'package:flutter/material.dart';
 
+final _rowHeight = 100.0;
+final _borderRadius = BorderRadius.circular(_rowHeight/2);
 /// A custom [Category] widget.
 ///
 /// The widget is composed on an [Icon] and [Text]. Tapping on the widget shows
@@ -15,8 +17,14 @@ class Category extends StatelessWidget {
   ///
   /// A [Category] saves the name of the Category (e.g. 'Length'), its color for
   /// the UI, and the icon that represents it (e.g. a ruler).
+  ///
+  final IconData icon;
+  final Color color;
+  final String name;
+
+  ///
   // TODO: You'll need the name, color, and iconLocation from main.dart
-  const Category();
+  const Category(this.icon, this.color, this.name);
 
   /// Builds a custom widget that shows [Category] information.
   ///
@@ -28,6 +36,29 @@ class Category extends StatelessWidget {
   // See https://docs.flutter.io/flutter/material/Theme-class.html
   Widget build(BuildContext context) {
     // TODO: Build the custom widget here, referring to the Specs.
-    return Container();
+    return Container(
+      height: _rowHeight,
+      padding: EdgeInsets.all(8.0),
+      child:
+      InkWell(
+        onTap: (){ print("I was tapped!");},
+        splashColor: color,
+        highlightColor: color,
+        borderRadius: _borderRadius,
+        child:
+        Row(
+          children: <Widget>[
+            Padding(padding: EdgeInsets.all(16.0), child: Icon(icon)),
+            Center(
+              child: Text(
+                this.name,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 24.0),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
